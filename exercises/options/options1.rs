cd200ca082b18417360fd3da3e34203ecebd2246
AnswerWong <andy_wong10@163.com>
2023-07-18 17:33:30 +0800

    Update options1.rs

--- exercises/options/options1.rs
+++ exercises/options/options1.rs
@@ -33,7 +33,10 @@ mod tests {
     #[test]
     fn raw_value() {
         // TODO: Fix this test. How do you get at the value contained in the Option?
-        let icecreams = maybe_icecream(12).unwrap();
-        assert_eq!(icecreams, 5);
+        // let icecreams = maybe_icecream(12).unwrap();
+        // assert_eq!(icecreams, 5);
+        if let Some(ice) = maybe_icecream(12) {
+            assert_eq!(ice, 5);
+        }
     }
 }
